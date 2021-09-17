c207be53721dff9e69db7ef2fec2a85dffec0186
Arjuna Sky Kok <arjuna@mamba.black>
2021-09-17 12:44:45 +0700

    List all products in the index controller.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get products_url
+    assert_response :success
+    assert_equal "index", @controller.action_name
+    assert_match "Product1", @response.body
+    assert_match "Product2", @response.body
+  end
 end
