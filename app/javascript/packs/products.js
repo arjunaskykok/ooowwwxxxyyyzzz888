3bcbcc389a2882b5a66c38048b6ae3df5ed16c7f
Arjuna Sky Kok <arjuna@mamba.black>
2021-09-17 16:30:52 +0700

    Add callback to the button.

---
+++ app/javascript/packs/products.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  let add_review_button = document.getElementById("add-review");
+  add_review_button.addEventListener("click", loadUpReviewForm);
+});
+
+function loadUpReviewForm() {
+  console.log("Button clicked")
+}
+
