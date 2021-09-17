45e28f3583e29301166c60361f33db496eb9874f
Arjuna Sky Kok <arjuna@mamba.black>
2021-09-17 14:54:29 +0700

    Round the average rating number.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
 
   def reviews
     @product = Product.find(params[:id])
-    @ratings_average = Review.where(product_id: @product.id).average(:stars)
+    @ratings_average = Review.where(product_id: @product.id).average(:stars).round(2)
     @reviews = @product.reviews.order(created_at: :desc)
   end
 end
