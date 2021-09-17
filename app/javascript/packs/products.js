2578021ae4eafe7d75893a35f9318c46a29d8143
Arjuna Sky Kok <arjuna@mamba.black>
2021-09-17 16:37:41 +0700

    Add a modal form to submit a review.

--- app/javascript/packs/products.js
+++ app/javascript/packs/products.js
@@ -1,9 +1,18 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   let add_review_button = document.getElementById("add-review");
   add_review_button.addEventListener("click", loadUpReviewForm);
+
+  let modal_background = document.getElementById("review-form");
+  modal_background.addEventListener("click", hideReviewForm);
 });
 
 function loadUpReviewForm() {
-  console.log("Button clicked")
+  let add_review_form = document.getElementById("review-form");
+  add_review_form.style.display = "block";
 }
 
+function hideReviewForm(event) {
+  if (event.target.id=="review-form") {
+    event.target.style.display = "none";
+  }
+}
