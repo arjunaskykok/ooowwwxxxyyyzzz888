47e3f43c6dcb2fb542cbecfdf2539be1ba71ceb1
Arjuna Sky Kok <arjuna@mamba.black>
2021-09-17 15:54:12 +0700

    Add stars to the page.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,16 @@
 module ProductsHelper
+  def print_stars(num)
+    num = num.to_int
+    stars_str = ""
+    if num > 5 then
+      num = 5
+    end
+    num.times do
+      stars_str += render("full_star")
+    end
+    (5 - num).times do
+      stars_str += render("empty_star")
+    end
+    stars_str.html_safe
+  end
 end
