c0e49467e1d39dec5f6dd4028308c33151b147d3
Arjuna Sky Kok <arjuna@mamba.black>
2021-09-17 11:38:15 +0700

    Add test for the presence of the product title.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save product without title" do
+    product = Product.new
+    assert_not product.save, "Saved the product without a title"
+  end
 end
